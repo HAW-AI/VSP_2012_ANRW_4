5b51bf2a38904c002af4cb35386cdd922f758d56
raimundwege <raimundwege@web.de>
2012-12-19 12:43:22 +0100

    add comments

--- tools.erl
+++ tools.erl
@@ -2,14 +2,22 @@
 -compile(export_all).
 -author("Raimund Wege").
 
+%% the current frame is the current second
 getCurrentFrame()->
 	{_,Secs,_}=>now(),
 	Secs.
-    
+
+%% the current time in milliseconds as timestamp
+%% 1 megasecond are 1000000 seconds
+%% 1 second are 1000000 microseconds
+%% 1 microsecond div 1000 are 1000 milliseconds
 getTimestamp()->
     {MegaSecs,Secs,MicroSecs}=now(),
     ((MegaSecs*1000000+Secs)*1000000+MicroSecs) div 1000.
 
+%% the current slot for a time in milliseconds
+%% 1000 is a full second in milliseconds
+%% 50 is the length of a slot in milliseconds
 getSlotForMsec(Time)->
     trunc(((Time rem 1000)/50)).
 
